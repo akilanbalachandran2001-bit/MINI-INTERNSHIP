d7a062a63b21900071bebbedc5ba12e3e4634f90
akilanbalachandran2001-bit <akilanbalachandran2001@gmail.com>
2025-10-09 16:35:36 +0530

    Add files via upload

---
+++ DAY-3 HOME TASK (08-10-25).py
@@ -0,0 +1,12 @@
+#Task1
+a=int(input("Enter number for a"))
+b=int(input("Enter number for b"))
+c=a*b
+if c%15==0:
+    print("divisible by 15")
+else:
+    print("not divisible by 15")
+#task2
+a=[]
+a=input("enter a string to reverse it ")
+print(a[::-1])
